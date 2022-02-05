066cd6dca6453e53fd8dae292907869e13d7d5b5
horsevad <nikolajhorsevad@gmail.com>
2022-02-04 22:36:07 -0500

    first ltm main script and aux

---
+++ LTM_main.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+## Script for running the generating Small-world graphs, analyzing their network properties and running the Linear Threshold Model
+import os
+import numpy as np
+import time
+import graph_tool as gt
+import random
+
+from network_functions import ws_network
+from network_functions import get_recursive_graph_paths
+from network_functions import linear_threshold_model
+from network_functions import get_local_clutsering
+from network_functions import get_transitivity
+from network_functions import get_laplacian_eigenvalues
+from network_functions import get_kirchhoff_index
+from network_functions import get_ave_shortest_path
+## File used to generate netwokrs, and subsequently do the full analysis
+old_graph_list = []
+#Parameters for networks
+network_type = 'ws'
+nodes=[10000]
+average_degree=[16]
+## Thresholds are chosen such that they approximately give the thresholds of [1,...,8]
+thresholds = np.linspace(0.01,0.5,16)
+
+## How many realizations to do of each set of parameters
+desired_realizations= 1
+## How many unique starting points to run the LTM from on a network
+unique_network_seeds = 1
+## The rewirering probabilities to be used in the watts-strogatz model
+## We use from 10**-3 to 1 to get the full range of the Watts-Strogatz networks
+probabilities = np.append([0], np.logspace(-3,-0,10))
+## Root directory for storing networks
+network_root = '/home/horsevad/projects/networks/'
+
+## Make a dict with the probabilities as keys, to count the available networks
+realization_counter=dict.fromkeys(probabilities,0)
+##Cascade sizes of interest
+cascades = np.round(np.linspace(0.1,0.9,9),1)
+##Loop over entries in nodes and degrees, to create the desired networks
+for N in nodes:
+    for k in average_degree:
+        #Seed path for saving networks, put the
+        network_path = f"{network_root}{N}/{k}/{network_type}"
+        ## If the network_path does not exist it is created
+        if not os.path.exists(network_path):
+            os.makedirs(network_path)
+
+        # Count the graphs in network_path and create missing networks according to  parameters (N,k,p)
+        for graph_path in get_recursive_graph_paths(network_path):
+            g_old = gt.load_graph(str(graph_path))
+            realization_counter[g_old.gp.probability] += 1
+            if realization_counter[g_old.gp.probability] < 5:
+                g_old.save(f'networks/{g_old.gp.ID}.gt')
+        #### Create missing networks ####
+        for p in realization_counter:
+            ## Because only one network exists in the Watts-Strogatz
+            ## model for p=0 wer have a special case. Having many instances of the same network
+            ## will obscure the correlation results later
+            if p == 0:
+                if realization_counter[p] > 0:
+                    ## Makes the rest of the code not create any further p=0 networks
+                    realization_counter[p] = desired_realizations
+                else:
+                    realization_counter[p] = desired_realizations-1
+            # Too many networsk of a given probability has already been created
+            # This might obscure the correlation results later
+            if realization_counter[p] > desired_realizations:
+                print(f'too many networks of p={p}')
+            # Create new networks, until the disired number of realizations is achieved
+            elif realization_counter[p] < desired_realizations:
+                ## Loop until the desired number of networks is created
+                while realization_counter[p] < desired_realizations:
+                    G = ws_network(N,k,p,seed=None)
+                    # Add relevant creation properties with the graph.
+                    # Name graphs by their creation time in seconds since the epoc. This should ensure unique filenames
+                    G.graph_properties['ID'] = G.new_graph_property('int64_t',val=int(time.time()*1000))
+                    G.graph_properties['ntype'] = G.new_graph_property('string',val=network_type)
+                    G.graph_properties['probability'] = G.new_graph_property('double',p)
+
+                    G.save(f'{network_path}/{G.gp.ID}.gt')
+                    realization_counter[p] += 1
+
+        #### Calculate relevant network metrics and the LTM for the seeds ####
+        net_count = 0
+        for graph_path in list(get_recursive_graph_paths(network_path))[:]:
+            G = gt.load_graph(str(graph_path))
+
+            print(f'analysis of {G.gp.ID}')
+            ## Run the network analyzer function on all the networks
+            G = get_local_clutsering(G)
+            G = get_transitivity(G)
+            G = get_kirchhoff_index(G)
+            G = get_ave_shortest_path(G)
+
+
+            ## run the liner thershold model on the model, for an appropriate number of times
+            print(f'Running LTM on {G.gp.ID}')
+            ## Check if any seeds existes, and compute the LTM such that a sufficient number of unique_seeds was used
+            old_seeds = G.gp.get('seeds',False)
+
+            # if they exist place them in a list
+            if old_seeds:
+
+                ## Only nessesary if not sure wether seeds is properly formated, and takes a single integer seed and makes it to a list
+                seeds = [[s] if type(s) is not list else s for s in old_seeds]
+            else:
+                seeds = []
+            # Find the missing seeds
+            new_picks  = unique_network_seeds - len(seeds)
+            if new_picks > 0:
+                if old_seeds:
+                    # If old runs exist make a ocpty to be merged with ne w runs later
+                    all_spreads = gt.ungroup_vector_property(G.vp['infected_step'],range(len(seeds)*len(G.gp.thresholds)))
+                else:
+                    all_spreads = []
+
+                #Calculate new unique seeds different from previous seeds, if desired_seeds exceed nudes, only compute all nodes as seeds
+                new_seeds = []
+                if (G.num_vertices() - len(seeds)) >= new_picks:
+                    new_seeds = random.sample(list(np.delete(G.get_vertices(),seeds)),new_picks)
+                    print('New seeds added')
+                else:
+                    new_seeds = np.delete(G.get_vertices(),seeds)
+                    print('Full, preforming last possible seeds')
+
+                #Running of the LTM on the newly selected seeds
+                print('#### new runs p={},k={} ###'.format(np.round(G.gp.probability,4),np.max(G.get_out_degrees(new_seeds))))
+                for seed in new_seeds:
+                    spreads,_,thresholds_map = linear_threshold_model(G,thresholds,seed_nodes=[seed],max_iter=N,init_spread=True)
+
+                    all_spreads += gt.ungroup_vector_property(spreads,range(len(thresholds)))
+                    seeds.append(seed)
+                    # Merge the spreading vectors
+                    new_spreads = gt.group_vector_property(all_spreads)
+                # Add new properties ot the graph and save
+                G.vp['infected_step'] = new_spreads
+                G.gp['thresholds'] = thresholds_map
+                G.gp['seeds'] = G.new_gp(value_type='python::object',val=seeds)
+
+            else:
+                print(f'No new seeds')
+
+            #### Calculate polarization speeds and save them to seed vertices ###
+            ## fetch if exists; othterwise create
+            print('calculating polarization speeds')
+            if 'polarization_speed' not in G.vp:
+                polarization_speeds = np.full((G.num_vertices(),len(cascades)*len(thresholds)),-1.0)
+                G.vp['polarization_speed'] = G.new_vp(value_type='vector<double>',vals=polarization_speeds)
+
+            polarization_speeds = G.vp.polarization_speed.get_2d_array(range(len(cascades)*len(thresholds))).T
+
+            for idy,s in enumerate(seeds):
+                # Hack to get around index [0] or [0][0]
+                first = polarization_speeds[s]
+                while isinstance(first,np.ndarray):
+                    first = first[0]
+                if  first != -20:
+                    # print('calc')
+                    #initialize lits to aggregate the polarization speeds
+                    speeds = []
+                    # load simulation data
+                    spread = G.vp.infected_step.get_2d_array(np.arange(idy*len(thresholds),(idy+1)*len(thresholds)))
+                    for idx,th in enumerate(G.gp.thresholds):
+                        cascade_sizes = cascades ## Copy where we can shorten list on iterations in while loop
+                        infected = 0             ## Reset on loop start
+
+                        ## Find nodes infected at given step
+                        val,counts = np.unique(spread[idx],return_counts=True)
+                        ## Refactor to fraction of nodes
+                        counts = counts / G.num_vertices()
+                        ## For each step in LTM add newly infected to total
+                        for i,new in enumerate(counts[val>-2]):
+                            infected += new
+                            ## Current number of infected are used to find polarization speed if larger than cascade size, and the step is after initialization, and all cascade sizes has not been exceeded yet.
+                            while len(cascade_sizes) > 0 and infected > cascade_sizes[0] and val[i] > 0:
+                                speeds.append(infected/val[i])
+                                cascade_sizes = cascade_sizes[1:]
+                        for j in cascade_sizes:
+                            speeds.append(-1)
+                    polarization_speeds[s]=speeds
+
+            G.vp['polarization_speed'] = G.new_vp(value_type='vector<double>',vals=polarization_speeds)
+            G.gp['cascades'] = G.new_gp(value_type='python::object',val=cascades)
+            G.save(f'{network_path}/{G.gp.ID}.gt')
+            net_count+= 1
+            print(net_count)
---
+++ network_functions.py
@@ -0,0 +1,135 @@
+import graph_tool.all as gt
+import graph_tool.spectral
+import graph_tool.generation
+import numpy as np
+import networkx as nx
+import scipy as sp
+from pathlib import Path
+from scipy import sparse
+
+## Remove these later
+
+def ws_network(N,k,p,seed=None):
+    """
+    Function for creating a Modified Holme-Kim network
+    Takes inputs:
+       N: int, Number of nodes
+       k: integer, The mean degree of nodes
+       p: Probability of rewireing of edges on the graph
+       seed: Integer for the numpy random seed function
+    Returnd:
+       G: a graphtool graph
+
+    """ 
+    if seed is None:
+        seed=np.random.randint(2**63)
+
+    G = gt.Graph(directed=False)
+    G.add_edge_list(np.transpose(sp.sparse.tril(nx.adjacency_matrix(nx.connected_watts_strogatz_graph(N,k,p,tries=1000000,seed=seed))).nonzero()))
+
+    return G
+
+def linear_threshold_model(G,threshold,seed_nodes=None,init_spread=True,max_iter=None):
+    """
+    Runs the linear threshold model on a grap_too
+l graph G. S_i(t+1) = {1 if <s_j>i~j > T otherwise 0. If the average state of neighbours of i is more than the threshold T, switch the state from 0 to 1
+
+    Takes inputs:
+       G: Graph_tool graph
+       threshold: float or list of the thresholds as a fraction of neighbors that need to be active to transmit
+       seed_nodes: int or list of nodes to start infection from, if None a single random agent is choosen. If int, a number of random nodes is choosen, if a list of nodes those are the initial seeds.
+       init_spread: Bool, Wether to spread the infection to the neighbors of seed nodes
+       max_iter = maximum number of iterations before stopping. If all nodes in the graph are active the model will stop
+    Returnd:
+       infected_step: Graph_tool VertexPropertyMap with an interger denoting the step of activation for a given vertex, None if never activated. Velues are stored as vectors, with each index corresponding to a given threshold.
+       seed_nodes: Graph_tool GraphProperty with the initail seed nodes for the model run
+
+    """
+
+    if seed_nodes == None:
+        [seed_nodes for x in np.random.choice(G.get_vertices(),1)]
+
+    if not type(seed_nodes) is list:
+        seed_nodes = np.random.choice(G.get_vertices(),seed_nodes)
+
+    if max_iter is None:
+        max_iter = G.num_vertices()
+        
+    if not type(threshold) is list:
+        [threshold]
+
+    infections = []
+    degree_dist = G.get_out_degrees(G.get_vertices())
+
+    T = np.array((graph_tool.spectral.adjacency(G).T / degree_dist).T)  
+
+    for th in threshold:
+        # Choose the initial infected nodes
+        infected = np.zeros(G.num_vertices(),dtype=int)
+        infection_step = np.full(G.num_vertices(),np.inf,dtype=float)
+        node_list = np.arange(G.num_vertices(),dtype=int)
+        
+        #Infect the seed nodes
+        infected[seed_nodes] = 1
+        #Record seed nodes infected at t=-1
+        infection_step[seed_nodes] = -1
+
+        #Initial spread, if choosen
+        if init_spread:
+            infected[T.dot(infected) > 0] = 1
+            infection_step[np.logical_and(infected > 0, np.isinf(infection_step))] = 0
+            i = 1
+        else:
+            i = 0
+
+        while (not all(infected) and (i < max_iter) and i-1 in infection_step):
+            infected[T.dot(infected) >= th] = 1
+            infection_step[np.logical_and(infected > 0, np.isinf(infection_step))] = i
+
+            i += 1
+        
+        infected_step = G.new_vp(value_type='int',vals=infection_step)
+        infections.append(infected_step)
+        
+    
+    infected_vectormap = gt.group_vector_property(infections)
+    # G.vp['infected_step'] = infected_vectormap    
+    threshold_vector = G.new_gp(value_type='vector<double>',val=threshold)
+
+
+    return infected_vectormap,seed_nodes,threshold_vector
+
+
+def get_laplacian_eigenvalues(G):
+    """
+    """
+
+    if not G.vertex_properties.get('eig_laplacian',False):
+        eig_lap = np.linalg.eigvalsh(gt.laplacian(G,norm=False).todense())
+        G.vp['eig_laplacian'] =  G.new_vertex_property('double',vals=eig_lap)
+    return G
+
+def get_kirchhoff_index(G):
+    G = get_laplacian_eigenvalues(G)
+    G.graph_properties['kirchhoff'] = G.new_graph_property('int64_t',sum(1/np.sort(G.vp.eig_laplacian.get_array())[1:]))
+    return G
+
+def get_recursive_graph_paths(root):
+    paths = Path(Path(root)).rglob('*.gt')
+    return paths
+
+def get_local_clutsering(G):
+    if not G.vertex_properties.get('local_clustering',False):
+        G.vertex_properties['local_clustering'] =  gt.local_clustering(G)
+    return G
+
+def get_transitivity(G):
+    if not G.gp.get('transitivity',False):
+        trans = G.new_graph_property('double',val=gt.global_clustering(G)[0])
+        G.graph_properties['transitivity'] =  trans
+    return G
+
+def get_ave_shortest_path(G):
+    if not G.gp.get('shortest_path',False):
+        G.gp['shortest_path'] =  G.new_graph_property('double',val=np.sum(gt.shortest_distance(G).get_2d_array(range(G.num_vertices())))/(G.num_vertices()*(G.num_vertices()-1)))
+    return G
